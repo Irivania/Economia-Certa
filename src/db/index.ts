4e2444c6a798039ebd2d373c12a319be9eb50739
Irivânia Maria de Melo <v.melomaria@gmail.com>
2026-08-27 10:58:58 -0300

    chore: atualiza estrutura do projeto e usa variaveis de ambiente

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,11 @@ import { Pool } from 'pg';
 import * as schema from './schema';
 
 const pool = new Pool({
+<<<<<<< HEAD
   connectionString: "postgresql://postgres:@#PFIM22@$86@db.zbmlzqjhdicvgayzlfap.supabase.co:5432/postgres",
+=======
+  connectionString: process.env.DATABASE_URL,
+>>>>>>> e9c4a5b (chore: atualiza estrutura do projeto e usa variaveis de ambiente)
 });
 
 export const db = drizzle(pool, { schema });
